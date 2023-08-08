ac09eb95706e75b00c6117fc22fa8dc6bf56f6ad
umamaheswari76 <umamahes1301@gmail.com>
2023-08-08 14:50:26 +0530

    done with package get

--- Go_Tasks/Credit_validation/credit_vald.go
+++ Go_Tasks/Credit_validation/credit_vald.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
-
+import (
+	"fmt"
+	l "github.com/retgits/creditcard"
+)
 func main(){
-	
+	c := l.Card{"Visa", "969696969696", 12, 2023, "564"}
+	validation := c.Validate()
+    fmt.Printf("%+v\n", validation)
+    fmt.Printf("%+v\n", validation.Card)
+
 }
---
+++ Go_Tasks/go.mod
@@ -0,0 +1,5 @@
+module Go_Tasks
+
+go 1.20
+
+require github.com/retgits/creditcard v0.6.0 // indirect
