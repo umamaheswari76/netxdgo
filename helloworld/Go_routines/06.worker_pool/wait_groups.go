9ae10059b73ceaef62993583b5c3641c06710697
umamaheswari76 <umamahes1301@gmail.com>
2023-08-10 15:33:28 +0530

    workpool

---
+++ helloworld/Go_routines/06.worker_pool/wait_groups.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func routine(i int, wg *sync.WaitGroup){
+	fmt.Println("Started Routine", i)
+	//time.Sleep(time.Second*2)
+	fmt.Printf("Routine %d ended \n",i)
+	wg.Done()
+}
+
+func main(){
+	noOfRoutines := 3
+	var wg sync.WaitGroup
+	for i:=0;i<noOfRoutines;i++{
+		wg.Add(1)
+		go routine(i,&wg)
+	}
+	wg.Wait();
+	fmt.Println("All routines are finished")
+}
+
+
